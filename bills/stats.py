2a6e4079ec32ff2057be87c7f241a4fbe402fe34
rcresponiuoc <rcresponi@uoc.edu>
2026-06-17 00:19:33 +0200

    revisado y funcional

--- bills/stats.py
+++ bills/stats.py
@@ -25,7 +25,7 @@ class Statistics:
     def find_top_two_sellers(self) -> list:
         ventas = {}
         for bill in self.bills:
-            seller = bill.seller:
+            seller = bill.seller
             importe = bill.calculate_total()
             if seller in ventas:
                 ventas[seller] += importe
@@ -44,10 +44,22 @@ class Statistics:
                 compras[buyer] = importe
         clave = sorted(compras, key=compras.get)[0]      
         return (clave, compras[clave])
-    
+                     
     def order_products_by_tax(self, order_type: OrderType) -> tuple:
-        # Write here your code
-        pass
+        productos = {}
+        for bill in self.bills:
+            for product in bill.products:
+                impuestos = product.calculate_total_taxes()
+                if product in productos:
+                    productos[product] += impuestos
+                else:
+                    productos[product] = impuestos
+        if order_type == OrderType.ASC:
+            orden =  False
+        else:
+            orden = True     
+        claves = sorted(productos, key=productos.get, reverse=orden)                
+        return [(clave, productos[clave]) for clave in claves]
 
     def show(self):
         # Do not change this method
